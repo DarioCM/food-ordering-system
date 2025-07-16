a5b2dbde1de4b20421a72a36fa3c6ac9f3b34314
Carlos Castañeda <dario20049@gmail.com>
2025-07-17 00:21:39 +0900

    domain dto's for create

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+
+import com.food.ordering.system.domain.valueObject.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderResponse {
+
+  @NotNull
+  private final UUID orderTracking;
+
+  @NotNull
+  private final OrderStatus orderStatus;
+
+  @NotNull
+  private final String message;
+
+
+}
